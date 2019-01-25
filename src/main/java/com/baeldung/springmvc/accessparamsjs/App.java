b2cccfa124c9dd45fe7da4adcb59f203c7dec5ad
Andrey Shcherbakov <veontomo@gmail.com>
2019-01-25 07:07:09 +0100

    Set up the project

---
+++ src/main/java/com/baeldung/springmvc/accessparamsjs/App.java
@@ -0,0 +1,13 @@
+package com.baeldung.springmvc.accessparamsjs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
